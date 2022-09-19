3c7cc7c7a0f5fef6128e59958815000931c47536
rosh-1228 <sho.watanabe1228@gmail.com>
2022-09-19 13:08:29 +0900

    dontributorコントローラーを作成

---
+++ db/migrate/20220919040247_create_contributors.rb
@@ -0,0 +1,17 @@
+class CreateContributors < ActiveRecord::Migration[6.1]
+  def change
+    create_table :contributors do |t|
+      t.string  :name               , index: true
+      t.string  :avatar_url
+      t.date    :first_committed_on
+      t.integer :rank
+      t.integer :today_commits
+      t.integer :this_week_commits
+      t.integer :this_month_commits
+      t.integer :this_year_commits
+      t.integer :all_time_commits
+
+      t.timestamps
+    end
+  end
+end
