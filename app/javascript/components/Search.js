de150c90b1ac02914012086cc179ffb7a5d36f15
rosh-1228 <sho.watanabe1228@gmail.com>
2022-09-26 17:07:02 +0900

    検索ボタンをaタグからinputタグに修正

--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -10,11 +10,39 @@ class Search extends React.Component {
     this.setState({name : event.target.value})
   }
 
+  searchNameurl(){
+    location.href='/contributors/search?name='+this.state.name
+  }
+
   render() {
     return (
-      <div className="entry">
-        <div> <input type="search" value={this.state.name} name="name" onChange={(e) => this.onChangeName(e)} placeholder="ユーザー名を入力して検索" /> </div>
-        <div><a href={'/contributors/search?name='+this.state.name}>検索</a></div>
+      <div className="searchContributor">
+        <form>
+          <div>
+            <input
+              type="search"
+              value={this.state.name}
+              name="name"
+              onChange={e => this.onChangeName(e)}
+              onKeyPress={e =>
+                {
+                  if (e.key === 'Enter') {
+                    e.preventDefault()
+                    this.searchNameurl()
+                  }
+                }
+              }
+              placeholder="ユーザー名を入力して検索"
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              onClick={() => this.searchNameurl()}
+              value="検索"
+            />
+          </div>
+        </form>
       </div>
     )
   }
