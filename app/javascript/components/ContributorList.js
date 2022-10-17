645da9b1d51fa203a82d7731a8311e55b2736841
rosh-1228 <sho.watanabe1228@gmail.com>
2022-10-17 16:21:36 +0900

    commit一覧ページのデザインを実装

--- app/javascript/components/ContributorList.js
+++ app/javascript/components/ContributorList.js
@@ -25,7 +25,10 @@ const Contributors = (props) => {
 
       <table className="contributors_table">
         <thead>
-          <tr><th className='table_header'>  </th><th className='table_header'>Name</th><th className='table_header'>Commits</th></tr>
+          <tr>
+            <th className='table_header'>  </th>
+            <th className='table_header'>Name</th>
+            <th className='table_header'>Commits</th></tr>
         </thead>
         <tbody>
           {props.ranks.map((contributor) =>
@@ -44,20 +47,20 @@ const ContributorInfo = (props) => {
   const {rank, avatar_url, path, name, first_committed_on, commits} = props.contributor
   const width = {width: (commits/8)+'px'}
   return (
-    <tr className="contributor_table-row">
+    <tr className="contributors_table-row">
       <td className='table_data_rank'>#{rank}</td>
       <td className='table_data_info' >
-        <div className='contributor_info'>
-          <img className='contributor_avatar' src={avatar_url} />
+        <div className='contributors_info'>
+          <img className='contributors_avatar' src={avatar_url} />
           <div className='contributor_name_since'>
-            <a className='contributor_name' href={path}>  {name}  </a>
-            <p className='contributor_since'>since {first_committed_on}</p>
+            <a className='contributors_name' href={path}>  {name}  </a>
+            <p className='contributors_since'>since {first_committed_on}</p>
           </div>
         </div>
       </td>
       <td className='table_data_graph'>
-        <span className="contributor_horizontal_bar" style={width}>
-          <p className='contributor_commits'>{commits}</p>
+        <span className="contributors_horizontal_bar" style={width}>
+          <p className='contributors_commits'>{commits}</p>
         </span>
       </td>
     </tr>
