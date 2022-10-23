4535a14f6a4ac387359dcccf61fedd18f7790660
rosh-1228 <sho.watanabe1228@gmail.com>
2022-10-23 14:25:26 +0900

    production環境を起動できるように修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server_enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -119,4 +120,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.react.variant = :production
   config.hosts << "fjord-boot-camp-contributors-production.up.railway.app/"
+
+  # when local start
+  #config.hosts << '0.0.0.0'
 end
