167a94eab758f4dd7f349e4c39d0b638939a06b1
rosh-1228 <sho.watanabe1228@gmail.com>
2022-10-26 21:38:51 +0900

    棒グラフのwidthを1位は100%にし、2位以下は1位との比率で表示するように修正

--- app/javascript/components/ContributorList.js
+++ app/javascript/components/ContributorList.js
@@ -32,7 +32,7 @@ const Contributors = (props) => {
         </thead>
         <tbody>
           {props.ranks.map((contributor) =>
-            <ContributorInfo contributor={contributor} key={contributor.id} /> )}
+            <ContributorInfo contributor={contributor} key={contributor.id} topCommits={props.ranks[0].commits} /> )}
         </tbody>
       </table>
     </div>
@@ -45,7 +45,7 @@ Contributors.propTypes = {
 
 const ContributorInfo = (props) => {
   const {rank, avatar_url, path, name, first_committed_on, commits} = props.contributor
-  const width = (commits >= 100) ? { width: '100%'} : { width: commits+'%'}
+  const width = (rank === 1) ? { width: '100%'} : { width: ((commits/props.topCommits)*100)+'%'}
 
   return (
     <tr className="contributors_table-row">
