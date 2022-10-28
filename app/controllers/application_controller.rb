efea4c142c4266f6529a586db9c6e20909756e20
rosh-1228 <sho.watanabe1228@gmail.com>
2022-10-28 14:21:58 +0900

    CIの動作確認

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@
 
 class ApplicationController < ActionController::Base
   helper_method :all_contributors
+
+  def test_method
+    #This is a method for CI confirmation.
+    # It is turned off as soon as the confirmation is finished. 
+  end
 end
