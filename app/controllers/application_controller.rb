a75aa6c10914f23b9c4d288104d611b642973b36
rosh-1228 <sho.watanabe1228@gmail.com>
2022-10-29 11:08:23 +0900

    テスト用メソッドなので、削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@
 
 class ApplicationController < ActionController::Base
   helper_method :all_contributors
-
-  def test_method
-    # This is a method for CI confirmation.
-    # It is turned off as soon as the confirmation is finished.
-  end
 end
