c2eff6b0c7a3119d91c92abef4872a0a40ee0a85
rosh-1228 <sho.watanabe1228@gmail.com>
2022-10-29 09:57:07 +0900

    lintテスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   helper_method :all_contributors
 
   def test_method
-    #This is a method for CI confirmation.
+    # This is a method for CI confirmation.
     # It is turned off as soon as the confirmation is finished. 
   end
 end
