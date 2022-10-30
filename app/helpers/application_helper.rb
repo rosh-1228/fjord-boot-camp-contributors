66604eb17fd65d711393c1add480e9d95151fa77
rosh-1228 <sho.watanabe1228@gmail.com>
2022-10-30 21:49:10 +0900

    ページごとにtitleとdescriptionが変わるように変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'FjordBotCamp CONTRIBUTORS', title: 'FjordBotCamp CONTRIBUTORS',
+      site: 'FjordBotCamp CONTRIBUTORS', title: "FjordBotCamp CONTRIBUTORS #{change_title}",
       reverse: true, charset: 'utf-8',
-      description: 'フィヨルドブートキャンプ アプリのリポジトリに対するcommit数を集計し、ランキングを表示するサイトです。',
+      description: "フィヨルドブートキャンプ アプリのリポジトリに対する#{change_description}commit数を集計し、ランキングを表示するサイトです。",
       keywords: 'FjordBotCamp,Contributors,Ruby', canonical: request.original_url,
       separator: '|',
       icon: [
@@ -22,5 +22,26 @@ module ApplicationHelper
       },
       twitter: { card: 'summary_large_image', site: '@rosh_1228', image: image_url('twitter-card-img.png') }
     }
+  end
+
+  private
+
+  def change_title
+    params[:period].nil? ? 'This-month' : params[:period].capitalize
+  end
+
+  def change_description
+    case params[:period]
+    when 'this-month', nil
+      '今月の'
+    when 'this-year'
+      '今年の'
+    when 'this-week'
+      '今週の'
+    when 'today'
+      '今日の'
+    when 'all-time'
+      '最初のcommitからの'
+    end
   end
 end
