6c2c2478c579a5f31206a4d59ce31938b5e6fe45
rosh-1228 <sho.watanabe1228@gmail.com>
2022-10-31 04:59:16 +0900

    モーダル表示のスマホデザインを実装

--- app/javascript/components/Description.js
+++ app/javascript/components/Description.js
@@ -3,8 +3,8 @@ import Modal from 'react-modal'
 
 const customStyles = {
   content : {
-    top                   : '50%',
-    left                  : '50%',
+    top                   : '40%',
+    left                  : '40%',
     right                 : 'auto',
     bottom                : 'auto',
     marginRight           : 'auto',
@@ -39,28 +39,30 @@ class Description extends React.Component {
           isOpen={this.state.modalIsOpen}
           onRequestClose={this.closeModal}
           style={customStyles}
-          contentLabel="Modal"
+          contentLabel='Modal'
         >
-          <button className='delete' onClick={this.closeModal}></button>
-          <h2 className='modal_this_site_title' ref={subtitle => this.subtitle = subtitle}>フィヨルドブートキャンプ生のコミットランキングサイト</h2>
-          <p className='modal_sentence'>fjord boot camp contributorは、フィヨルドブートキャンプで行うチーム開発プラクティス(正式には、開発に参加してPRを送りマージする)にて、<a href='https://github.com/fjordllc/bootcamp'>リポジトリ</a>に所属するContributorのcommit数を集計し、ランキングを表示するサイトです。</p>
-          <h3 className='modal_sub-title'>このサイトの使い方</h3>
-          <ul>
-            <li className='modal_sentence'>就職活動にて、企業様に自身がフィヨルドブートキャンプのチーム開発で書いたコード一覧を提出できます。</li>
-            <li className='modal_sentence'>フィヨルドブートキャンプ内で他の人がどのようなコミットを行ったか、どのくらいのコミット(貢献)を行ったがわかります。</li>
-          </ul>
-          <h3 className='modal_sub-title'>ランキング更新タイミング</h3>
-          <p className='modal_sentence'>毎日 0時 6時 12時 18時 の4回、ランキングを更新します。</p>
-          <h3 className='modal_sub-title'>操作について</h3>
-          <p className='modal_sentence'>各Contributorのnameをクリックすると、そのContributorが実施したコミット一覧を表示します。</p>
-          <p className='modal_sentence'>検索ボックスにContributorのnameを入力することで、Contirbutorの検索もできます。</p>
-          <h3 className='modal_sub-title'>作者について</h3>
-          <p>rosh-1228</p>
-          <ul className='modal_outer_links'>
-            <li className='modal_outer_link'><a href='https://github.com/rosh-1228/fjord-boot-camp-contributors'><img src='/assets/GitHub-Mark-120px-plus.png' width="30" height="30" /></a></li>
-            <li className='modal_outer_link'><a href='https://twitter.com/rosh_1228'><img src='/assets/Twittersocialicons.png' width="30" height="30" /></a></li>
-            <li className='modal_outer_link'><a href='https://rosh-1228.hatenablog.com/'><img src='/assets/hatenablog-logo.svg' width="30" height="30" /></a></li>
-          </ul>
+          <div className='modal_description-about-site'>
+            <button className='delete' onClick={this.closeModal}></button>
+            <h2 className='modal_this_site_title' ref={subtitle => this.subtitle = subtitle}>フィヨルドブートキャンプ生のコミットランキングサイト</h2>
+            <p className='modal_sentence'>fjord boot camp contributorは、フィヨルドブートキャンプで行うチーム開発プラクティス(正式には、開発に参加してPRを送りマージする)にて、<a href='https://github.com/fjordllc/bootcamp'>リポジトリ</a>に所属するContributorのcommit数を集計し、ランキングを表示するサイトです。</p>
+            <h3 className='modal_sub-title'>このサイトの使い方</h3>
+            <ul>
+              <li className='modal_sentence'>就職活動にて、企業様に自身がフィヨルドブートキャンプのチーム開発で書いたコード一覧を提出できます。</li>
+              <li className='modal_sentence'>フィヨルドブートキャンプ内で他の人がどのようなコミットを行ったか、どのくらいのコミット(貢献)を行ったがわかります。</li>
+            </ul>
+            <h3 className='modal_sub-title'>ランキング更新タイミング</h3>
+            <p className='modal_sentence'>毎日 0時 6時 12時 18時 の4回、ランキングを更新します。</p>
+            <h3 className='modal_sub-title'>操作について</h3>
+            <p className='modal_sentence'>各Contributorのnameをクリックすると、そのContributorが実施したコミット一覧を表示します。</p>
+            <p className='modal_sentence'>検索ボックスにContributorのnameを入力することで、Contirbutorの検索もできます。</p>
+            <h3 className='modal_sub-title'>作者について</h3>
+            <p className='modal_author-name'>rosh-1228</p>
+            <ul className='modal_outer_links'>
+              <li className='modal_outer_link'><a href='https://github.com/rosh-1228/fjord-boot-camp-contributors'><img src='/assets/GitHub-Mark-120px-plus.png' width="30" height="30" /></a></li>
+              <li className='modal_outer_link'><a href='https://twitter.com/rosh_1228'><img src='/assets/Twittersocialicons.png' width="30" height="30" /></a></li>
+              <li className='modal_outer_link'><a href='https://rosh-1228.hatenablog.com/'><img src='/assets/hatenablog-logo.svg' width="30" height="30" /></a></li>
+            </ul>
+          </div>
         </Modal>
       </div>
     )
