06410217d2b8110924042a0bae0062f92afaea23
rosh-1228 <sho.watanabe1228@gmail.com>
2022-11-04 13:47:01 +0900

    create_array_contributors_commitsメソッドのif文のロジックを修正

--- lib/batch/create_array.rb
+++ lib/batch/create_array.rb
@@ -3,13 +3,15 @@
 module ContributorCommitArray
   def create_array_contributors_commits(fetched_commits, contributors, commits)
     fetched_commits.each do |node|
+      avatar_url = nil
+      login_name = node['node']['author']['name']
       if node['node']['author']['user']
-        contributors << [node['node']['author']['user']['login'], node['node']['author']['user']['avatarUrl']]
-        commits << [node['node']['oid'], node['node']['author']['date'].to_date, node['node']['message'], node['node']['author']['user']['login']]
-      else
-        contributors << [node['node']['author']['name'], nil]
-        commits << [node['node']['oid'], node['node']['author']['date'].to_date, node['node']['message'], node['node']['author']['name']]
+        avatar_url = node['node']['author']['user']['avatarUrl']
+        login_name = node['node']['author']['user']['login']
       end
+
+      contributors << [login_name, avatar_url]
+      commits << [node['node']['oid'], node['node']['author']['date'].to_date, node['node']['message'], login_name]
     end
   end
 end
