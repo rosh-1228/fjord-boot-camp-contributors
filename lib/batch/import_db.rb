a2fbf28e2ca937c92eaf5e7e48d3632cbfbc902e
rosh-1228 <64620506+rosh-1228@users.noreply.github.com>
2022-11-17 15:31:59 +0900

    更新ロジックを修正したので、テスト

--- lib/batch/import_db.rb
+++ lib/batch/import_db.rb
@@ -15,15 +15,13 @@ module ImportDB
     change_name_to_id(commits)
     all_commits_count = Commit.all.count
     repo_commits_count = commits.count
-
-    p 'all_commits_count'
-    p all_commits_count
-    p 'repo_commits_count'
-    p repo_commits_count
-    p 'all_commits_count < repo_commits_count'
-    p all_commits_count < repo_commits_count
     
-    commits.shift(Commit.all.count) if all_commits_count < repo_commits_count
+    #commits.shift(Commit.all.count) if all_commits_count < repo_commits_count
+    for_deleted_dupulicated_array = []
+    Commit.all.each do |commit|
+      for_deleted_dupulicated_array << [commit.hash, commit.committed_on, commit.message, commit.contributor_id]
+    end
+    commits = commits - for_deleted_dupulicated_array if all_commits_count < repo_commits_count
     p 'all_commits_count >= repo_commits_count'
     p all_commits_count >= repo_commits_count
     p 'commits'
