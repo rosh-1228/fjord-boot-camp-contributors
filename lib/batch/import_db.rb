5aad64c73ee83134c90731fec62fc3e5d711aeec
rosh-1228 <64620506+rosh-1228@users.noreply.github.com>
2022-11-17 16:43:27 +0900

    commitsデータが更新されていることを確認したので修正

--- lib/batch/import_db.rb
+++ lib/batch/import_db.rb
@@ -16,18 +16,11 @@ module ImportDB
     all_commits_count = Commit.all.count
     repo_commits_count = commits.count
     
-    #commits.shift(Commit.all.count) if all_commits_count < repo_commits_count
     for_deleted_dupulicated_array = []
     Commit.all.each do |commit|
       for_deleted_dupulicated_array << [commit.hash, commit.committed_on, commit.message, commit.contributor_id]
     end
     commits = commits - for_deleted_dupulicated_array if all_commits_count < repo_commits_count
-    p 'all_commits_count'
-    p all_commits_count
-    p 'repo_commits_count'
-    p repo_commits_count
-    p 'commits.count'
-    p commits.count
 
     Commit.import %i[hash committed_on message contributor_id], commits unless all_commits_count >= repo_commits_count
   end
